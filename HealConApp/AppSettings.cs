5097105da2c3448ec90b4e4fa01e46a8c27d7503
Jhenifer Cebrian <jheny_2007fofux@hotmail.com>
2024-12-15 13:48:58 -0300

    Ajuste de layout

--- HealConApp/AppSettings.cs
+++ HealConApp/AppSettings.cs
@@ -3,6 +3,6 @@ namespace HealConApp
 {
     public static class AppSettings
     {
-        public const string ApiBaseUrl = "https://localhost:44319/"; 
+        public const string ApiBaseUrl = "https://healconapi-e9a9htc2abdpa3f6.brazilsouth-01.azurewebsites.net/"; 
     }
 }
